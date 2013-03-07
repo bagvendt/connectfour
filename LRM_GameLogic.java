afca06e923e87e702aea33e05ba1fdc7881fe3d1
RickMarker <rickmarker@gmail.com>
2013-03-07 16:14:51 +0100

    Added decideNextMove()

--- LRM_GameLogic.java
+++ LRM_GameLogic.java
@@ -37,8 +37,80 @@ public class LRM_GameLogic implements IGameLogic {
 	 */
 	@Override
 	public int decideNextMove() {
-		// TODO Auto-generated method stub
-		return 1;
+		
+		long time = System.currentTimeMillis();
+		int depth = 2;
+		int v = 0;
+		while(time < 9000){ // LESS THAN 9000 !
+			v = maxValue(gameBoard,depth,Integer.MAX_VALUE,Integer.MIN_VALUE);
+			depth++;
+		}
+		
+		return v;
+	}
+	
+	private int maxValue(Board state,int depth,int alpha,int beta){
+		
+		switch (state.gameFinished()){
+		case Winner.PLAYER1:
+			return ourPlayer == Winner.PLAYER1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+			break;
+			
+		case Winner.PLAYER2:
+			return ourPlayer == Winner.PLAYER2 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+			break;
+			
+		case Winner.TIE:
+			return 0;
+			break;
+			
+		case Winner.NOT_FINISHED:
+			
+			if (depth == 0) return state.evalute();
+			
+			int v = Integer.MIN_VALUE;
+			for (int column : state.actions()){ // Should be arranged according to values from previous iteration
+				
+				Board newState = state.copy();
+				newState.layBrick(new Brick(ourPlayer), column);
+				v = Math.max(v,minValue(newState,depth-1,alpha,beta));
+				if (v >= beta) return v;
+				alpha = Math.max(v,alpha);
+			}
+			return v;
+		}
+	}
+	
+	private int minValue(Board state,int depth,int alpha,int beta){
+		
+		switch (state.gameFinished()){
+			case Winner.PLAYER1:
+				return ourPlayer == Winner.PLAYER1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+				break;
+				
+			case Winner.PLAYER2:
+				return ourPlayer == Winner.PLAYER2 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+				break;
+				
+			case Winner.TIE:
+				return 0;
+				break;
+				
+			case Winner.NOT_FINISHED:
+				
+				if (depth == 0) return state.evalute();
+				
+				int v = Integer.MAX_VALUE;
+				for (int column : state.actions()){ // Should be arranged according to values from previous iteration
+					Board newState = state.copy();
+					newState.layBrick(new Brick(ourPlayer), column);
+					v = Math.min(v,maxValue(newState,depth-1,alpha,beta));
+					if (v <= alpha) return v;
+					alpha = Math.min(v,beta);
+				}
+				return v;
+				break;
+		}
 	}
 
 	/* (non-Javadoc)
