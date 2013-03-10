08dd63e8e60f2955339d837e697efc774cf48d23
RickMarker <rickmarker@gmail.com>
2013-03-11 00:03:09 +0100

    Misc

--- Board.java
+++ Board.java
@@ -1,129 +1,207 @@
 import java.util.ArrayList;
 import java.util.*;
 
-public class Board implements Cloneable {
+public class Board{
 	
-	private ArrayList<Brick>[] board;
-	private ArrayList<Brick> bricks;
+	private Stack<IGameLogic.Winner>[] board;
+	private Stack<Integer> history;
 	private int height, length;
+	private IGameLogic.Winner finished;
 	
 	/**
 	 * @param board
 	 */
 	public Board(int columns, int rows) {
-		length = columns;
-		height = rows;
-		board = new ArrayList[columns];
-		for (int i = 0; i < board.length; i++) {
-			board[i] = new ArrayList<Brick>();
-		}
-		bricks = new ArrayList<Brick>();
-	}
-	 public Object clone(){
-		  try{
-			  Board cloned = (Board)super.clone();
-			  return cloned;
-		  }
-		  catch(CloneNotSupportedException e){
-			  System.out.println(e);
-		  return null;
-		  }
-		  }
-	public void layBrick(Brick brick, int atColumn) {
-		ArrayList<Brick> column = board[atColumn];
-		column.add(brick);
-		int height = column.size();
-		// Column is zero indexed by design, so better make the row zero indexed too.
-		brick.setRow(height);
-		brick.setColumn(atColumn);
-		bricks.add(brick);
-	}
-	
-	
-	public Brick getBrickRelative(Brick brick, Compass direction) {
-		int col = brick.getColumn();
-		int row = brick.getRow() -1;
+		length = columns-1;
+		height = rows-1;
+		history = new Stack<Integer>();
 		
-		switch (direction) {
-		case N:
-			row = row +1;
-			break;
-		case NE:
-			row = row + 1;
-			col = col + 1;
-			break;
-		case E:
-			col = col + 1;
-			break;
-		case SE:
-			row = row + 1;
-			col = col - 1;
-			break;
-		case S:
-			row = row - 1;
-			break;
-		case SW:
-			row = row + 1;
-			col = col + 1;
-			break;
-		case W:
-			col = col + 1;
-			break;
-		case NW:
-			row = row + 1;
-			col = col - 1;
-			break;
+		board = new Stack[columns];
+		for (int i = 0; i < board.length; i++) {
+			board[i] = new Stack<IGameLogic.Winner>();
 		}
-		if (col > height || 0 > row) {
-			return null;
-		}
-		if (row > length || 0 > row) {
-			return null;
-		}
-		ArrayList<Brick> theCol = board[col];
-		if (row > theCol.size()) {
-			return null;
-		}
-		System.out.println(row);
-		return theCol.get(col);
 	}
 
-	public IGameLogic.Winner gameFinished() {
-		// TODO Auto-generated method stub
-		for (Brick b : bricks) {
-			
-		}
-		return IGameLogic.Winner.NOT_FINISHED;
+	public void layBrick(int column,IGameLogic.Winner player) {
+		board[column].push(player);
+		history.push(column);
+		updateGameFinished();
+	}
+
+	public void removeLastBrick() {
+		int lastColumn = history.pop();
+		board[lastColumn].pop();
+		updateGameFinished();
 	}
 	
-	public Boolean checkWinner(Brick brick) {
-		for(Compass direction: Compass.values()) {
-			Brick brick1 = getBrickRelative(brick, direction);
-			Brick brick2 = getBrickRelative(brick1, direction);
-			Brick brick3 = getBrickRelative(brick2, direction);
-			if (brick.getPlayer() == brick1.getPlayer() && brick1.getPlayer() == brick2.getPlayer() && brick2.getPlayer() == brick3.getPlayer()) {
-				return true;
+	public IGameLogic.Winner gameFinished() {
+		return finished;
+	}
+	
+	public int evaluate(IGameLogic.Winner ourPlayer){
+		int utility = 0;
+		switch (finished){
+			case PLAYER1:
+				utility = ourPlayer == IGameLogic.Winner.PLAYER1 ? Integer.MAX_VALUE-1 : Integer.MIN_VALUE+1;
+				break;
+				
+			case PLAYER2:
+				utility = ourPlayer == IGameLogic.Winner.PLAYER2 ? Integer.MAX_VALUE-1 : Integer.MIN_VALUE+1;
+				break;
+				
+			case TIE:
+				
+			case NOT_FINISHED:
+				
+				utility = 0;
+				break;
+		}
+		return utility;
+	}
+	
+	private boolean positionIsValid(int column,int row, IGameLogic.Winner player){
+		
+		// Outside range of the board
+		if (column < 0 || column > length || row < 0 || row > height) return false;
+		
+		// space not taken by coin
+		if (board[column].size()-1 < row) return false;
+		
+		// coin is position is not players;
+		if(board[column].get(row) != player) return false;
+		
+		// If nothing has stopped you from getting here, this is your coin
+		return true;
+	}
+	
+	public boolean isGameFinished(){
+		return finished != IGameLogic.Winner.NOT_FINISHED;
+	}
+
+	private void updateGameFinished() {
+		
+		int lastCoinX = history.peek();
+		int lastCoinY = board[lastCoinX].size()-1;
+		IGameLogic.Winner player = board[lastCoinX].get(lastCoinY);
+		
+		int NE = 0;
+		int E  = 0;
+		int SE = 0;
+		int S  = 0;
+		int SW = 0;
+		int W  = 0;
+		int NW = 0;
+		
+		// Going through coins. Direction: NE
+		for (int i = 1; i <=3; i++){
+			if (positionIsValid(lastCoinX+i,lastCoinY+i,player))
+				NE++;
+			else
+				break;
+		}
+		
+		// Going through coins. Direction: SW 
+		for (int i = 1; i <=3; i++){
+			if (positionIsValid(lastCoinX-i,lastCoinY-i,player))
+				SW++;
+			else
+				break;
+		}
+		
+		if(NE + SW + 1 >= 4){
+			finished = player;
+			return;
+		}
+		
+		// Going through coins. Direction: E
+		for (int i = 1; i <=3; i++){
+			if (positionIsValid(lastCoinX+i,lastCoinY,player))
+				E++;
+			else
+				break;
+		}
+		
+		// Going through coins. Direction: W 
+		for (int i = 1; i <=3; i++){
+			if (positionIsValid(lastCoinX-i,lastCoinY,player))
+				W++;
+			else
+				break;
+		}
+		
+		if(E + W + 1 >= 4){
+			finished = player;
+			return;
+		}
+		
+		// Going through coins. Direction: SE
+		for (int i = 1; i <=3; i++){
+			if (positionIsValid(lastCoinX-i,lastCoinY+i,player))
+				SE++;
+			else
+				break;
+		}
+		
+		// Going through coins. Direction: NW 
+		for (int i = 1; i <=3; i++){
+			if (positionIsValid(lastCoinX+i,lastCoinY-i,player))
+				NW++;
+			else
+				break;
+		}
+		
+		if(SE + NW + 1 >= 4){
+			finished = player;
+			return;
+		}
+		
+		// Going through coins. Direction: S
+		for (int i = 1; i <=3; i++){
+			if (positionIsValid(lastCoinX,lastCoinY-i,player))
+				S++;
+			else
+				break;
+		}
+		
+		if(S + 1 >= 4){
+			finished = player;
+			return;
+		}
+			
+		
+		
+		
+		boolean tie = true;
+		
+		for (int i = 0; i <= length; i++){
+			if (board[i].size()-1 < height){
+				tie = false;
+				break;
 			}
 		}
-		return false;
-	}
-
-	public int evalute() {
-		return 0;
 		
+		if (tie){
+			finished = IGameLogic.Winner.TIE;
+		}
+		
+		finished = IGameLogic.Winner.NOT_FINISHED;
 	}
-
+	
 	public Set<Integer> actions() {
 		Set<Integer> intSet = new HashSet<Integer>(length); 
+		
 		int column;
-		for (Brick brick : bricks){
-			column = brick.getColumn();
-			for(int j = -3; j <= 3; j++){
-				if (column+j > 0 && column+j < length && board[column+j].size() != height)
-					intSet.add(column+j);
+		for (int i = 0; i <= length; i++){
+			if (!board[i].isEmpty()){
+				for(int j = -3; j <= 3; j++){
+					column = i+j;
+					
+					if (column >= 0 && column <= length && board[column].size()-1 != height)
+						intSet.add(column);
+				}
 			}
 		}
-		if (intSet.size() == 0) intSet.add(length/2);
+		if (intSet.size() == 0 && finished != IGameLogic.Winner.TIE) intSet.add(length/2);
 		
 		return intSet;
 	}
