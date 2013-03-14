0c8c9b76efc48d4b5ef0d7b0723697efcc68f2bf
lasseastrup <lasseastrup@gmail.com>
2013-03-14 14:25:46 +0100

    MERGE FIX LORT SØD ÆSD

--- Board.java
+++ Board.java
@@ -1,163 +1,256 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Board {
+
+	private Stack<IGameLogic.Winner>[] board;
+	private Stack<Integer> history;
+	private int height, length;
+	private IGameLogic.Winner finished;
+	private IGameLogic.Winner ourPlayer;
+	private int eval;
 	
-	private ArrayList<Brick>[] board;
-	private ArrayList<Brick> bricks;
-	public int Height; 
-	public int Length;
+
 	/**
 	 * @param board
 	 */
-	public Board(int columns, int rows) {
-		Height = rows;
-		Length = rows;
-		//KUSSE
-		board = new ArrayList[columns];
-		for (int i = 0; i < board.length; i++) {
-			board[i] = new ArrayList<Brick>();
-		}
-		bricks = new ArrayList<Brick>();
-	}
-	
-	public int EvaluateBoard(IGameLogic.Winner player) 
-	{
-		int h = 0;
+
+	public Board(int columns, int rows,IGameLogic.Winner ourPlayer) {
+		eval = 0;
+		length = columns-1;
+		height = rows-1;
+		history = new Stack<Integer>();
+		finished = IGameLogic.Winner.NOT_FINISHED;
 		
-		for (Brick b : bricks) 
-		{
-			int col = b.getColumn();
-			int row = b.getRow();
-			
-			// W
-			if (col > 2) 
-			{
-				if (GetNeighbours(row, col, -1,0, player) == 4)
-					h++;
-			}
-			
-			// NW
-			if (col > 2 && row < Height-4) 
-			{
-				if (GetNeighbours(row, col, -1, 1, player) == 4)
-					h++;
-			}
-			
-			// N
-			if (row < Height-4) 
-			{
-				if (GetNeighbours(row, col, 0, 1, player) == 4)
-					h++;
-			}
-			
-			// NE
-			if (row < Height-4 && col < Length-4) 
-			{
-				if (GetNeighbours(row, col, 1, 1, player) == 4)
-					h++;
-			}
-			
-			// E
-			if (col < Length-4) 
-			{
-				if (GetNeighbours(row, col, 1, 0, player) == 4)
-					h++;
-			}
-			
-			// SE
-			if (row > 2 && col < Length-4) 
-			{
-				if (GetNeighbours(row, col, -1, 1, player) == 4)
-					h++;
-			}
-			
-			// S
-			if (row > 2) 
-			{
-				if (GetNeighbours(row, col, -1, 0, player) == 4)
-					h++;
-			}
-			
-			// SW
-			if (row > 2 && col > 2) 
-			{
-				if (GetNeighbours(row, col, -1, -1, player) == 4)
-					h++;
-			}
-		}	
-		return h;
-	}
-	
-	private int GetNeighbours(int playerRow, int playerCol, int horizontalIncrementer, int verticalIncrementer, IGameLogic.Winner player) 
-	{
-		int neighbours = 0;
-		for (int i = 1; i < 4; i++) 
-		{
-			int colIndex = playerCol + i * horizontalIncrementer;
-			int rowIndex = playerRow + i * verticalIncrementer;
-			if (board[colIndex].size() > rowIndex && board[colIndex].get(rowIndex).getPlayer() != player)
-				break;
-			neighbours++;
+		this.ourPlayer = ourPlayer;
+		
+		board = new Stack[columns];
+		for (int i = 0; i < board.length; i++) {
+			board[i] = new Stack<IGameLogic.Winner>();
 		}
-		return neighbours;
 	}
 
-	public void layBrick(Brick brick, int atColumn) {
-		ArrayList<Brick> column = board[atColumn];
-		column.add(brick);
-		int height = column.size();
-		// Column is zero indexed by design, so better make the row zero indexed too.
-		brick.setRow(height);
-		brick.setColumn(atColumn);
-		bricks.add(brick);
+	public void layCoin(int column, IGameLogic.Winner player) {
+		board[column].push(player);
+		history.push(column);
+		updateGameFinished();
 	}
-	
-	public IGameLogic.Winner goalState() {
-		for (Brick b: bricks) {
-			//
-			
+
+	public void removeLastCoin() {
+		int lastColumn = history.pop();
+		board[lastColumn].pop();
+		updateGameFinished();
+	}
+
+	public IGameLogic.Winner gameFinished() {
+		return finished;
+	}
+
+	public int evaluate() {
+
+		int utility = 0;
+		switch (finished) {
+		case PLAYER1:
+			utility = ourPlayer == IGameLogic.Winner.PLAYER1 ? Integer.MAX_VALUE - 1
+					: Integer.MIN_VALUE + 1;
+			break;
+
+		case PLAYER2:
+			utility = ourPlayer == IGameLogic.Winner.PLAYER2 ? Integer.MAX_VALUE - 1
+					: Integer.MIN_VALUE + 1;
+			break;
+
+		case TIE:
+
+		case NOT_FINISHED:
+			utility += EuclidianDistance();
+			break;
 		}
-		return IGameLogic.Winner.PLAYER1;
+		return utility;
 	}
-	public Brick getBrickRelative(Brick brick, Compass direction) {
-		int col = brick.getColumn();
-		int row = brick.getRow();
+
+	private int EuclidianDistance() 
+	{
+		double myDistance = 0, otherDistance = 0;
+		int midX = (int)(length/2);
+		int midY = (int)(height/2);
+		int col = 0, row = 0;
+		for (Stack<IGameLogic.Winner> column : board)
+		{
+			for (IGameLogic.Winner winner : column) {
+				double distance = Math.sqrt(row*row-midX*midX+col*col-midY*midY);
+				if (winner.equals(ourPlayer)) 
+				{
+					myDistance += distance;
+				} else 
+				{
+					otherDistance += distance;
+				}
+				row++;
+			}
+		col++;
+		}
 		
-		switch (direction) {
-		case N:
-			row = row +1;
-			break;
-		case NE:
-			row = row + 1;
-			col = col + 1;
-			break;
-		case E:
-			col = col + 1;
-			break;
-		case SE:
-			row = row + 1;
-			col = col - 1;
-			break;
-		case S:
-			row = row - 1;
-			break;
-		case SW:
-			row = row + 1;
-			col = col + 1;
-			break;
-		case W:
-			col = col + 1;
-			break;
-		case NW:
-			row = row + 1;
-			col = col - 1;
-			break;
-		}
-		//Brick b = board[-1][-1];
-		return null;
+		if (myDistance < otherDistance) return 1;
+		else if (otherDistance > myDistance) return -1;
+		return 0;
 	}
-	
-	
-	
+
+	private boolean positionIsValid(int column, int row,
+			IGameLogic.Winner player) {
+
+		// Outside range of the board
+		if (column < 0 || column > length || row < 0 || row > height)
+			return false;
+
+		// space not taken by coin
+		if (board[column].size() - 1 < row)
+			return false;
+
+		// coin is position is not players;
+		if (board[column].get(row) != player)
+			return false;
+
+		// If nothing has stopped you from getting here, this is your coin
+		return true;
+	}
+
+	public boolean isGameFinished() {
+		return finished != IGameLogic.Winner.NOT_FINISHED;
+	}
+
+	private void updateGameFinished() {
+
+		if (history.size() == 0) {
+			finished = IGameLogic.Winner.NOT_FINISHED;
+			return;
+		}
+
+		int lastCoinX = history.peek();
+		int lastCoinY = board[lastCoinX].size() - 1;
+		IGameLogic.Winner player = board[lastCoinX].get(lastCoinY);
+
+		int NE = 0;
+		int E = 0;
+		int SE = 0;
+		int S = 0;
+		int SW = 0;
+		int W = 0;
+		int NW = 0;
+
+		// Going through coins. Direction: NE
+		for (int i = 1; i <= 3; i++) {
+			if (positionIsValid(lastCoinX + i, lastCoinY + i, player))
+				NE++;
+			else
+				break;
+		}
+
+		// Going through coins. Direction: SW
+		for (int i = 1; i <= 3; i++) {
+			if (positionIsValid(lastCoinX - i, lastCoinY - i, player))
+				SW++;
+			else
+				break;
+		}
+
+		if (NE + SW + 1 >= 4) {
+			finished = player;
+			return;
+		}
+
+		// Going through coins. Direction: E
+		for (int i = 1; i <= 3; i++) {
+			if (positionIsValid(lastCoinX + i, lastCoinY, player))
+				E++;
+			else
+				break;
+		}
+
+		// Going through coins. Direction: W
+		for (int i = 1; i <= 3; i++) {
+			if (positionIsValid(lastCoinX - i, lastCoinY, player))
+				W++;
+			else
+				break;
+		}
+
+		if (E + W + 1 >= 4) {
+			finished = player;
+			return;
+		}
+
+		// Going through coins. Direction: SE
+		for (int i = 1; i <= 3; i++) {
+			if (positionIsValid(lastCoinX - i, lastCoinY + i, player))
+				SE++;
+			else
+				break;
+		}
+
+		// Going through coins. Direction: NW
+		for (int i = 1; i <= 3; i++) {
+			if (positionIsValid(lastCoinX + i, lastCoinY - i, player))
+				NW++;
+			else
+				break;
+		}
+
+		if (SE + NW + 1 >= 4) {
+			finished = player;
+			return;
+		}
+
+		// Going through coins. Direction: S
+		for (int i = 1; i <= 3; i++) {
+			if (positionIsValid(lastCoinX, lastCoinY - i, player))
+				S++;
+			else
+				break;
+		}
+
+		if (S + 1 >= 4) {
+			finished = player;
+			return;
+		}
+
+		boolean tie = true;
+
+		for (int i = 0; i <= length; i++) {
+			if (board[i].size() - 1 < height) {
+				tie = false;
+				break;
+			}
+		}
+
+		if (tie) {
+			finished = IGameLogic.Winner.TIE;
+		}
+
+		finished = IGameLogic.Winner.NOT_FINISHED;
+	}
+
+	private boolean freeValidColumn(int column) {
+		return (column >= 0 && column <= length && board[column].size() - 1 != height);
+	}
+
+	public Set<Integer> actions() {
+		Set<Integer> intSet = new HashSet<Integer>(length); 
+		
+		int column;
+		for (int i = 0; i <= length; i++) {
+			if (!board[i].isEmpty()) {
+				for (int j = -3; j <= 3; j++) {
+					column = i + j;
+
+					if (freeValidColumn(column))
+						intSet.add(column);
+				}
+			}
+		}
+		if (intSet.size() == 0 && finished != IGameLogic.Winner.TIE)
+			intSet.add(length / 2);
+
+		return intSet;
+	}
 
 }
--- LRM_GameLogic.java
+++ LRM_GameLogic.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 
  */
@@ -15,11 +20,16 @@ public class LRM_GameLogic implements IGameLogic {
 	private Board gameBoard;
 	private IGameLogic.Winner ourPlayer;
 	private IGameLogic.Winner enemyPlayer;
+	private int decision;
+	private int decisionDepth;
+	
 	@Override
 	public void initializeGame(int columns, int rows, int player) {
-		gameBoard = new Board(columns,rows);
+		decision = -1;
 		ourPlayer = player == 1 ? IGameLogic.Winner.PLAYER1 : IGameLogic.Winner.PLAYER2;
 		enemyPlayer = player == 2 ? IGameLogic.Winner.PLAYER1 : IGameLogic.Winner.PLAYER2;
+		
+		gameBoard = new Board(columns,rows,ourPlayer);
 	}
 
 	/* (non-Javadoc)
@@ -28,8 +38,7 @@ public class LRM_GameLogic implements IGameLogic {
 	@Override
 	public void insertCoin(int column, int playerID) {
 		IGameLogic.Winner thePlayer = playerID == 1 ? IGameLogic.Winner.PLAYER1 : IGameLogic.Winner.PLAYER2;
-		Brick brick = new Brick(thePlayer);
-		gameBoard.layBrick(brick, column);	
+		gameBoard.layCoin(column, thePlayer);	
 	}
 
 	/* (non-Javadoc)
@@ -37,8 +46,77 @@ public class LRM_GameLogic implements IGameLogic {
 	 */
 	@Override
 	public int decideNextMove() {
-		// TODO Auto-generated method stub
-		return 1;
+		
+		long time = System.currentTimeMillis();
+		int maxDepth = 10;
+		System.out.println("Starting calculation");
+		while(maxDepth <= 10) {
+			decisionDepth = maxDepth;
+			maxValue(maxDepth,Integer.MIN_VALUE,Integer.MAX_VALUE);
+			maxDepth++;
+		}
+		System.out.println("Decision took (ms): " + Long.toString(System.currentTimeMillis()-time));
+		return decision;
+	}
+	
+	private int maxValue(int depth,int alpha,int beta){
+		
+		if (gameBoard.isGameFinished() || depth == 0) return gameBoard.evaluate();
+		
+		int v = Integer.MIN_VALUE;
+		int tempValue;
+		for (int column : gameBoard.actions()){ // Should be arranged according to values from previous iteration
+			
+			gameBoard.layCoin(column, ourPlayer);
+			tempValue = minValue(depth-1,alpha,beta) / 10 * 9; // Decreases value over time
+			//if (depth == decisionDepth) System.out.println("Column: " + Integer.toString(column) + "\nValue: " + Integer.toString(tempValue) + "\n");
+			gameBoard.removeLastCoin();
+			if (tempValue > v){
+				v = tempValue;
+				if (depth == decisionDepth)
+					decision = column;
+			}
+			
+			if (v >= beta){
+				System.out.println("Beta-cut");
+				System.out.println(v);
+				return v;
+			}
+			alpha = Math.max(v,alpha);
+		}
+		//System.out.println(v);
+		return v;
+	}
+	
+	private int minValue(int depth,int alpha,int beta){
+		
+		if (gameBoard.isGameFinished() || depth == 0){
+			return (gameBoard.evaluate());
+		}
+		
+		int v = Integer.MAX_VALUE;
+		int tempValue;
+		for (int column : gameBoard.actions()){ // Should be arranged according to values from previous iteration
+			
+			gameBoard.layCoin(column, enemyPlayer);
+			tempValue = maxValue(depth-1,alpha,beta) / 10 * 9; // Decreases value over time
+			gameBoard.removeLastCoin();
+			
+			if (tempValue < v){
+				v = tempValue;
+				if (depth == decisionDepth)
+					decision = column;
+			}
+			
+			if (v <= alpha){
+				System.out.println("Alpha-cut");
+				System.out.println(v);
+				return v;
+			}
+			beta = Math.min(v,beta);
+		}
+		//System.out.println(v);
+		return v;
 	}
 
 	/* (non-Javadoc)
@@ -46,8 +124,8 @@ public class LRM_GameLogic implements IGameLogic {
 	 */
 	@Override
 	public Winner gameFinished() {
-		// TODO Auto-generated method stub
-		return IGameLogic.Winner.NOT_FINISHED;
+
+		return gameBoard.gameFinished();
 	}
 
 }
