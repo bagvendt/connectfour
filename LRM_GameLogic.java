d99d1ae5b3f2451d36c089eb8b03c515c1362c43
Marcus Bjerg Gregersen <mgmano@gmail.com>
2013-03-18 13:23:54 +0100

    Javadoc slut

--- LRM_GameLogic.java
+++ LRM_GameLogic.java
@@ -1,8 +1,8 @@
 import java.util.List;
 
 /**
- * @author bagvendt
- *
+ * Our implementation of IGameLogic
+ * LRM = Lasse Lasse Rick Marcus :)
  */
 public class LRM_GameLogic implements IGameLogic {
 
@@ -17,6 +17,12 @@ public class LRM_GameLogic implements IGameLogic {
 	private int decisionDepth;
 	
 	@Override
+	/**
+	 * Initializes the game. Sets up a board and the ourPlayer, enemyPlayer enumerations.
+	 * @param columns Number of columns in the game.
+	 * @param rows Number of rows in the game.
+	 * @param player The player ID of our player.
+	 */
 	public void initializeGame(int columns, int rows, int player) {
 		
 		// Initialize decision to -1 so we get an error if something doesn't work
@@ -28,17 +34,20 @@ public class LRM_GameLogic implements IGameLogic {
 		gameBoard = new Board(columns,rows,ourPlayer, enemyPlayer);
 	}
 
-	/* (non-Javadoc)
-	 * @see IGameLogic#insertCoin(int, int)
-	 */
+
 	@Override
+	/**
+	 * Our implementation of insert coin. Inserts coin at column with playerID.
+	 * @param column the column where the coin should be placed
+	 * @param playerID the owner of the coin
+	 */
 	public void insertCoin(int column, int playerID) {
 		IGameLogic.Winner thePlayer = playerID == 1 ? IGameLogic.Winner.PLAYER1 : IGameLogic.Winner.PLAYER2;
 		gameBoard.layCoin(column, thePlayer);	
 	}
 
-	/* (non-Javadoc)
-	 * @see IGameLogic#decideNextMove()
+	/**
+	 * Returns where the next coin should be placed.
 	 */
 	@Override
 	public int decideNextMove() {
@@ -59,6 +68,13 @@ public class LRM_GameLogic implements IGameLogic {
 		return decision;
 	}
 	
+	/**
+	 * Our implementation of MAX, in the MINIMAX Decision algorithm
+	 * @param depth The current depth.
+	 * @param alpha The current alpha value.
+	 * @param beta The current beta value
+	 * @return The maximum guaranteed possible utility value
+	 */
 	private int maxValue(int depth,int alpha,int beta){
 		
 		// Should we stop and evaluate
@@ -102,6 +118,13 @@ public class LRM_GameLogic implements IGameLogic {
 		return v;
 	}
 	
+	/**
+	 * Our implementation of MIN, in the MINIMAX Decision algorithm
+	 * @param depth The current depth.
+	 * @param alpha The current alpha value.
+	 * @param beta The current beta value
+	 * @return The minimal guaranteed possible utility value
+	 */
 	private int minValue(int depth,int alpha,int beta){
 		
 		if (gameBoard.isGameFinished() || depth == 0) return gameBoard.evaluate();
@@ -139,8 +162,8 @@ public class LRM_GameLogic implements IGameLogic {
 		return v;
 	}
 
-	/* (non-Javadoc)
-	 * @see IGameLogic#gameFinished()
+	/**
+	 * Returns the game state IGameLogic enumeration 
 	 */
 	@Override
 	public Winner gameFinished() {
